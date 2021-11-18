d80a3c74d9467f2098efc46728fec5e0198d2781
Artur <xxarturkrossxx@yandex.ru>
2021-11-18 18:57:49 +0300

    add move to left

--- src/components/atoms/Ball/Ball.jsx
+++ src/components/atoms/Ball/Ball.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import './index.css'
 
+const moveDict = {
+    gotoRight2: "gotoRight2",
+    gotoRight3: "gotoRight3",
+    gotoRight4: "gotoRight4",
+    gotoRight5: "gotoRight5",
+    gotoRight6: "gotoRight6",
+    gotoRight7: "gotoRight7",
+    gotoRight8: "gotoRight8",
+    gotoRight9: "gotoRight9",
+    stay: "stay"
+}
+
 const Ball = (props) => {
     return (
-        <div className={props.color ==='green'? 'ball green': 'ball'}/>
+        <div className={props.color ==='green'? 'ball green': 'ball' + ' ' + moveDict[props.goto]}/>
     );
 };
+
 
 export default Ball;
